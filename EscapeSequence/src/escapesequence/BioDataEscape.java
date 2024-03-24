0ad40cefcfa7d14a0e02431ada1bdb475cd6f105
Ahafid <ahafidkabir11@gmail.com>
2024-03-24 12:37:27 +0600

    Add files via upload

---
+++ EscapeSequence/src/escapesequence/BioDataEscape.java
@@ -0,0 +1,13 @@
+
+package escapesequence;
+
+public class BioDataEscape {
+    public static void main(String[] args){
+        System.out.println("Ahafid Zaman \n 01786490426\n Aiub");// \n is new line
+        // ' \t tab 
+        System.out.println(" 1 \t 2 \n 3 \t 4 ");
+        System.out.println("\"1\"");// to print "" use \"
+        
+        
+    }
+}
